98cb3082b393937066295584d0896775ac4d8a7d
saichaitanya10 <saichaitanya8099@gmail.com>
2024-03-29 00:27:35 +0530

    ss

--- routes/route.js
+++ routes/route.js
@@ -1,10 +1,6 @@
 import express from 'express';
 const router = express.Router();
-<<<<<<< HEAD
 import { startContainer,pullImage,getMachineInfo,getDockerInfo, stopContainer,runContainer, listContainers ,listImages,getContainerStats} from '../controllers/controller.js';
-=======
-import { startContainer,pullImage,getMachineInfo,getDockerInfo, stopContainer,runContainer, listContainers ,listImages} from '../controllers/controller.js';
->>>>>>> 8c224361ea43447ad640f040046d82bd902b1d4a
 
 
 
@@ -29,10 +25,7 @@ router.get('/dockerInfo', getDockerInfo)
 
 router.post('/pullImage' , pullImage)
 
-<<<<<<< HEAD
 
 router.get('/stats/:containerId', getContainerStats);
 
-=======
->>>>>>> 8c224361ea43447ad640f040046d82bd902b1d4a
 export default router;
