3ddc12c8c4fc7f4f65e6ac5ac55cfa8d70350ebc
pavanpatchikarla <76902617+P-1000@users.noreply.github.com>
2024-03-29 04:25:09 +0530

    modules

--- routes/route.js
+++ routes/route.js
@@ -16,28 +16,22 @@ import {
   pushImageToHub,
 } from "../controllers/manageController.js";
 
-// Start a container
+
 router.get("/start/:id", startContainer);
 
-// Stop a container
+
 router.get("/stop/:id", stopContainer);
 
-// Get list of containers
 router.get("/allContainers", listContainers);
 
-// get list of all images
 router.get("/allImages", listImages);
 
-// start a container with image
 router.post("/createContainer", runContainer);
 
-//get machine info htop
 router.get("/machineInfo", getMachineInfo);
 
-// docker info for number of container and images
 router.get("/dockerInfo", getDockerInfo);
 
-// pull image from dockerhub
 router.post("/pullImage", pullImage);
 
 router.get("/stats/:containerId", getContainerStats);
